f75ae91e4a01b2fcc24faa0864f2f6464b7be30e
barabadi <101886037+barabadi@users.noreply.github.com>
2023-01-16 20:13:24 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,20 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const ul = document.querySelector("ul");
 });
+
+const pokemonCount = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?limit=${pokemonCount}`;
+const pokemonList = document.getElementById("pokemon-list");
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    data.results.forEach((pokemon) => {
+      const pokemonItem = document.createElement("li");
+      pokemonItem.innerText = pokemon.name;
+      pokemonList.appendChild(pokemonItem);
+    });
+  })
+  .catch((error) => {
+    console.error("Error fetching Pokemons:", error);
+  });
